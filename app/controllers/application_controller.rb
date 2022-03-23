c0b569aefe27ca0dad776725a871974f8d812345
EC2 Default User <ec2-user@ip-172-31-47-134.ec2.internal>
2022-03-23 23:02:19 +0000

    1st commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top, :about]
+
+  protected
+    def configure_permitted_parameters
+        if resource_class == User
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
+            devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
+            devise_parameter_sanitizer.permit(:account_update,keys:[:name,:email])
+        end
+    end
+end
